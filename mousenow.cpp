0832dcb0853b9253bbaa0158059e92ea049779ac
enderloong <yloongli@gmail.com>
2018-02-23 08:58:18 +0800

    ver 0.5: mouse event

--- mousenow.cpp
+++ mousenow.cpp
@@ -6,14 +6,42 @@
 #include "mainwindow.cpp"
 #endif
 
+#include <QDebug>
+#include <QRgb>
+
 bool MainWindow::eventFilter(QObject *watched, QEvent *event)
 {
-    if(watched == ui->graphicsView && event->type() == QEvent::MouseButtonPress){
+    bool isMouseEvent =
+            (event->type() == QEvent::MouseButtonPress) ||
+            (event->type() == QEvent::MouseButtonRelease) ||
+            (event->type() == QEvent::MouseMove)||
+            (event->type() == QEvent::MouseButtonDblClick) ||
+            (event->type() == QEvent::MouseTrackingChange);
+    if(watched == ui->graphicsView && isMouseEvent){
         QMouseEvent *mouseEvent = static_cast<QMouseEvent*> (event);
-        QString mousePos = QString("%1, %2").arg(mouseEvent->pos().x()).arg(mouseEvent->pos().y());
-        ui->statusBar->showMessage(mousePos);
+        QPoint mousePos = mouseEvent->pos();
+        ui->statusBar->showMessage(QString("%1").arg(mousePos.x()));
+        if(event->type() == QEvent::MouseButtonPress){
+            drawPix(mousePos.x(), mousePos.y());
+        }
     }
     return false;
+}
+
+void MainWindow::drawPix(int x, int y)
+{
+    this->mousex = x; this->mousey = y;
+    for(int h = -50; h< 50; h++){
+        for(int w = -50; w<50; w++){
+            image->setPixel(x+w, y+h, Qt::red);
+        }
+    }
+    image->setPixel(x, y ,Qt::red);
+    QPixmap imgarea = QPixmap::fromImage(*image);
+    QGraphicsScene *scene = new QGraphicsScene;
+    scene->addPixmap(imgarea);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->resize(image->width() + 10, image->height() + 10);
 }
 
 #endif
